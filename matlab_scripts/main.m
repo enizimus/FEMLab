13c080facf37e2b72a67f20aad44b1a11c5eca50
Eniz Mušeljić <eniz.m@outlook.com>
2019-02-17 22:54:42 +0100

    path fix

--- matlab_scripts/main.m
+++ matlab_scripts/main.m
@@ -5,7 +5,7 @@ close all
 pths = {'helper_functions', 'classes', ...
 'main_functions', 'solver_functions'};
 
-addpath(pths{1}, pths{2}, pths{3});
+addpath(pths{1}, pths{2}, pths{3}, pths{4});
 
 file_name = parse_gmesh();
 display_mesh(file_name,1)
