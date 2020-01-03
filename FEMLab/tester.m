4e3cd1630f4456a8069099fa5a9df1fbfed3d114
EnizMuseljic <eniz.m@outlook.com>
2020-01-03 09:29:44 +0100

    Remove redundant functions

--- FEMLab/tester.m
+++
@@ -1,9 +0,0 @@
-projPath = '/home/jpajser/Desktop/FEMDev/FEMLab/sample_projects/coil/';
-projName = 'coil';
-
-files = io.generateFiles(projPath, projName);
-
-elemOrder = 2;
-files = io.parseGmesh(files, elemOrder);
-msh.regionsSetup(files)
-
